123bc79bc997d0ead55018fc55deb458261b2e5f
agilles1 <adam.g.tpt@gmail.com>
2021-03-21 14:17:43 -0400

    update form js and html

--- frontend-client/public/index.js
+++ frontend-client/public/index.js
@@ -5,39 +5,19 @@ app.getTestTimes()
 app.getUnassignedPatients()
 saveButton.addEventListener("click", (event) => app.postNewAppointment(event))
 
-
-const openmodal = document.querySelectorAll('.modal-open')
+//Form JS 
+const openmodal = document.getElementById("create-apt-btn")
 const overlay = document.querySelector('.modal-overlay')
-const closemodal = document.querySelectorAll('.modal-close')
+const closemodal = document.getElementById('modal-close')
 
-for (let i = 0; i < openmodal.length; i++) {
-      openmodal[i].addEventListener('click', function(event){
-    	toggleModal()
-      })
-}
+openmodal.addEventListener('click', toggleModal)
 overlay.addEventListener('click', toggleModal)
-for (let i = 0; i < closemodal.length; i++) {
-      closemodal[i].addEventListener('click', toggleModal)
-    }
-    
-    document.onkeydown = function(evt) {
-      evt = evt || window.event
-      let isEscape = false
-      if ("key" in evt) {
-    	isEscape = (evt.key === "Escape" || evt.key === "Esc")
-      } else {
-    	isEscape = (evt.keyCode === 27)
-      }
-      if (isEscape && document.body.classList.contains('modal-active')) {
-    	toggleModal()
-      }
-    };
-    
-    
-    function toggleModal () {
-      const body = document.querySelector('body')
-      const modal = document.querySelector('.modal')
-      modal.classList.toggle('opacity-0')
-      modal.classList.toggle('pointer-events-none')
-      body.classList.toggle('modal-active')
-    }
+closemodal.addEventListener('click', toggleModal)
+
+function toggleModal () {
+  const body = document.querySelector('body')
+  const modal = document.querySelector('.modal')
+  modal.classList.toggle('opacity-0')
+  modal.classList.toggle('pointer-events-none')
+  body.classList.toggle('modal-active')
+}
