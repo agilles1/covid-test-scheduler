854ed296889e762adc680c6a682bfe8f73474cea
agilles1 <adam.g.tpt@gmail.com>
2021-06-13 18:30:33 -0400

    addPatient

--- frontend-client/public/index.js
+++ frontend-client/public/index.js
@@ -1,7 +1,11 @@
 const app = new AppContainer
 const saveButton = document.getElementById("create-new-apt-btn")
+const addPatient = document.getElementById("random-assign-btn")
 
 app.getTestTimes()
 app.getUnassignedPatients()
 saveButton.addEventListener("click", (event) => 
   app.postNewAppointment(event))
+
+addPatient.addEventListener("click", (event) =>
+  app.randomAssignPatient(event))
